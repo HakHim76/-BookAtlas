1f33a45374bded832da53fd939a4bee87f8bde18
Abdul Hakeem Bawah <abdulhakeembawah@gmail.com>
2026-03-06 07:56:12 +0000

    initial Author Routes setup

---
+++ routes/authors.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
+
+// SHOW ALL AUTHORS
+
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+
+  if (req.query.name != null && req.query.name != "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+// SHOW ONE AUTHOR
+
+// NEW  AUTHOR
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+// CREATE AN AUTHOR
+router.post("/", async (req, res) => {
+  const author = new Author({ name: req.body.name });
+
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect("/authors");
+  } catch (err) {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error Creating Author",
+    });
+  }
+});
+// // UPDATE AUTHOR
+// router.patch("/" (req,res), () => {
+
+// })
+// // DELETE AUTHOR
+// router.delete("/" (req,res), () => {
+
+// })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use((req, res, next) => {
+  res.locals.errorMessage = null;
+  next();
+});
 
 const indexRouter = require("./routes/index");
+const authorsRouter = require("./routes/authors");
+
 app.use("/", indexRouter);
+app.use("/authors", authorsRouter);
 
 const PORT = process.env.PORT;
 
